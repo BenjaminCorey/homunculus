1fe2730f18acfe73fafd8eb0d97457785c6d045d
Benjamin Corey Wilkins <becowilk@gmail.com>
2013-04-03 22:11:38 -0700

    Active Admin configuration

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Homunculus
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Don't access the database on asset precompilation (for Heroku)
+    config.assets.initialize_on_precompile = false
   end
 end
