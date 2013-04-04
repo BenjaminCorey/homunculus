bb6d5d84121eddf52332f3cd58e54b04fa94038a
Benjamin Corey Wilkins <becowilk@gmail.com>
2013-04-03 21:14:57 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homunculus::Application.config.secret_token = '39a95703a5258405e749b5d71b07590b764a76a325186e2de578774ccbb0c7d1490e8f13d313ecca0ec48218164df5b5582d82b12a9379648fbbcbf663c9721f'
